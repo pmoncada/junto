10ffec07d913c2fe450f9096246b899628d7ae8e
Cooper Sloan <cooper.sloan@gmail.com>
2020-10-24 17:30:17 -0700

    populate fields

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ Junto.prototype.resolveContractLender = function() {
     var that = this;
     // this.showLoader(true);
     //Sets message using the public update function of the smart contract
-    this.instance.resolveContractLender(
+    this.instance.resolveContract(
         {
             from: $('#lender').val(),
             gas: 100000,
@@ -190,7 +190,7 @@ Junto.prototype.resolveContractBorrower = function() {
     var that = this;
     // this.showLoader(true);
     //Sets message using the public update function of the smart contract
-    this.instance.resolveContractBorrower(
+    this.instance.resolveContract(
         {
             from: $('#borrower').val(),
             gas: 100000,
